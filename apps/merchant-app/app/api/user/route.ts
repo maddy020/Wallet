5acc5fa3199c9b0a519a83f81bafbf418472845b
Madhav Setia <madhavsetia24@gmail.com>
2024-04-24 16:57:22 +0530

    corrected builf

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -2,14 +2,6 @@ import { NextResponse } from "next/server";
 import client from "@repo/db/client";
 
 export const GET = async () => {
-  await client.user.create({
-    data: {
-      email: "asd",
-      name: "adsads",
-      number: "95178888",
-      password: "skfjkds",
-    },
-  });
   return NextResponse.json({
     message: "hi there",
   });
