69976c1e4b46e0d26875b3c9e90060df3118b8d6
Madhav Setia <madhavsetia24@gmail.com>
2024-04-24 15:53:34 +0530

    checking workflows

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -2,9 +2,12 @@
 
 import { useBalance } from "@repo/store/balance";
 
-export default function() {
+export default function () {
   const balance = useBalance();
-  return <div>
-    hi there {balance}
-  </div>
+  return (
+    <div>
+      hi there {balance}
+      <p>bvsdjvk</p>
+    </div>
+  );
 }
